6025e83adc7d70dc6d5f61fda414bce221e99711
Josh Nies <josh.nies@hey.com>
2022-12-26 15:55:02 -0500

    update styles

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,13 +3,6 @@ import "solid-styled-components";
 declare module "solid-styled-components" {
   export interface DefaultTheme {
     colors: {
-      primary: {
-        100: string;
-        200: string;
-        300: string;
-        400: string;
-        500: string;
-      };
       neutral: {
         0: string;
         50: string;
@@ -24,12 +17,36 @@ declare module "solid-styled-components" {
         900: string;
         1000: string;
       };
-      error: {
+      primary: {
         100: string;
         200: string;
         300: string;
         400: string;
         500: string;
+        600: string;
+        700: string;
+        800: string;
+        900: string;
+      };
+      info: {
+        100: string;
+        200: string;
+        300: string;
+      };
+      success: {
+        100: string;
+        200: string;
+        300: string;
+      };
+      warning: {
+        100: string;
+        200: string;
+        300: string;
+      };
+      error: {
+        100: string;
+        200: string;
+        300: string;
       };
     };
     shadows: {
