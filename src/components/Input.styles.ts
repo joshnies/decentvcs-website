a85e64c5dc12c0f8174bba3d0af0ce054f782da6
Josh Nies <josh.nies@hey.com>
2022-12-26 17:00:53 -0500

    fix style errors

--- src/components/Input.styles.ts
+++ src/components/Input.styles.ts
@@ -9,7 +9,7 @@ export const StyledInput = styled.input<InputProps>`
   width: 100%;
   background: ${({ theme }) => theme?.colors.neutral[100]};
   border: ${({ theme, error }) =>
-    error ? `2px solid ${theme?.colors.error[500]}` : "2px solid transparent"};
+    error ? `2px solid ${theme?.colors.error[300]}` : "2px solid transparent"};
   border-radius: 0.5rem;
   padding: 0.625rem 0.875rem;
   margin-top: 2rem;
@@ -18,7 +18,7 @@ export const StyledInput = styled.input<InputProps>`
   &:focus {
     border: ${({ theme, error }) =>
       error
-        ? `2px solid ${theme?.colors.error[500]}`
+        ? `2px solid ${theme?.colors.error[300]}`
         : "2px solid transparent"};
     outline: none;
 
@@ -34,7 +34,7 @@ export const StyledInput = styled.input<InputProps>`
 `;
 
 export const ErrorText = styled.p`
-  color: ${({ theme }) => theme?.colors.error[500]};
+  color: ${({ theme }) => theme?.colors.error[300]};
   font-size: 0.875rem;
   margin-top: 0.25rem;
 `;
