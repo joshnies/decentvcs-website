9d7c060d7dbd46cddb823b407ed7d20a6ab3bc7a
Josh Nies <josh.nies@hey.com>
2022-12-26 21:38:14 -0500

    improve hero spacing

--- src/pages/HomePage.styles.ts
+++ src/pages/HomePage.styles.ts
@@ -3,10 +3,9 @@ import { styled } from "solid-styled-components";
 export const Hero = styled.section`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   width: 100%;
-  height: calc(100vh - 74px - 32px);
+  margin-top: 15vh;
 `;
 
 export const HeroTitle = styled.h1`
