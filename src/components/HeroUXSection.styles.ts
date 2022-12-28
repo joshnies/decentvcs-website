4f5d8d718613ba211092d51b62b2ae3c4ffabd76
Josh Nies <josh.nies@hey.com>
2022-12-28 11:13:46 -0500

    hero ux section

--- src/components/HeroUXSection.styles.ts
+++ src/components/HeroUXSection.styles.ts
@@ -1,11 +1,37 @@
 import { styled } from "solid-styled-components";
 
 export const Root = styled.section`
+  display: flex;
+  justify-content: center;
   width: 100%;
-  height: 36rem;
   background: white;
 `;
 
 export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
   max-width: ${({ theme }) => theme?.maxWidth};
+  margin: 0 auto;
+  padding: 2rem 0;
+
+  color: ${({ theme }) => theme?.colors.neutral[1000]};
+`;
+
+export const TextBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 450px;
+
+  p {
+    margin-top: 2rem;
+  }
+`;
+
+export const Screen = styled.img`
+  width: 100%;
+  max-width: 800px;
+  height: 100%;
+  object-fit: contain;
 `;
--- src/components/HeroUXSection.tsx
+++ src/components/HeroUXSection.tsx
@@ -1,9 +1,24 @@
-import { Content, Root } from "./HeroUXSection.styles";
+import { Content, Root, Screen, TextBlock } from "./HeroUXSection.styles";
 
 export default function HeroUXSection() {
   return (
     <Root>
-      <Content></Content>
+      <Content>
+        <TextBlock>
+          <h4>Developer experience</h4>
+          <h2>Built from the ground up.</h2>
+          <p>
+            DecentVCS saves you an astounding amount on development costs by
+            utilizing decentralized storage by default. This also brings other
+            benefits such as an always-available data infrastructure with 100%
+            uptime, as well as enhanced security across the board.
+          </p>
+        </TextBlock>
+        <Screen
+          src="/src/assets/home-ux-4k.png"
+          alt="DecentVCS desktop app screenshot"
+        />
+      </Content>
     </Root>
   );
 }
